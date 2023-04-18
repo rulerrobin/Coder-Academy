0c959d284aa3b2221e086b5c8e3e76367e6d6eb9
Genuel Mark <gmzapanta98@gmail.com>
2023-04-18 17:57:58 +1000

    added python folder

---
+++ python/intro/hello.py
@@ -0,0 +1,32 @@
+
+# celsius = input()
+
+# fahrenheit = (int(celsius)*(9/5)) + 32
+
+# print (f"The result is: {fahrenheit}.")
+
+# celsius = input()
+
+# fahrenheit = (celsius*9/5) + 32
+
+# print (f"The result is: {fahrenheit}.")
+# =================================================
+
+# arr = [5, 22, 29, 39, 19, 51, 78, 96, 84]
+
+# i = 0
+
+# while (i<arr.len() -1) and (arr[i] < arr[i+1]):
+#      i += 1
+# print(i)
+#     arr[i] = arr[i+1]
+#     arr[i+1] = arr[i]
+
+arr = [5, 22, 29, 39, 19, 51, 78, 96, 84]
+
+i = 0
+
+while (i< len(arr) -1) and (arr[i] < arr[i+1]):
+     i += 1
+print(arr, i)
+
