f55f9814a310aa31e783853cca9d47f07eaf96f5
Genuel Mark <gmzapanta98@gmail.com>
2023-04-26 17:27:25 +1000

    8 ball with list vs else/if and match

---
+++ python/loops/magic8ball.py
@@ -0,0 +1,11 @@
+print('Magic 8 Ball')
+import random
+answers = [
+    'It is certain',
+    'It is decidedly so',
+    'Yes',
+    'Ask again later',
+    'Concentrate and ask again',
+    'Reply hazy try again'
+]
+# print(answers[random.randint(0,len(answers) - 1)])
