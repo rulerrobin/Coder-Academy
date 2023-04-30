7418326d0d4f1c353f3e8c03f4205a483c20342c
Genuel Mark <gmzapanta98@gmail.com>
2023-05-01 09:39:40 +1000

    T1A1 assignment done

--- term1/T1A1/Q16.py
+++ term1/T1A1/Q16.py
@@ -1,23 +1,48 @@
-# declare list of skills with a given score for each
-skills = [
-    {'code': 'python', 'score': 1},
-    {'code': 'ruby', 'score': 2},
-    {'code': 'bash', 'score': 4},
-    {'code': 'git', 'score': 8},
-    {'code': 'html', 'score': 16},
-    {'code': 'tdd', 'score': 32},
-    {'code': 'css', 'score': 64},
-    {'code': 'javascript', 'score': 128}
-]
+skills = {
+    'python': 1,
+    'ruby': 2,
+    'bash': 4,
+    'git': 8,
+    'html': 16,
+    'tdd': 32,
+    'css': 64,
+    'javascript': 128
+}
 
 # get input from user of skills
-selection = input ("What code skills do you have (nothing to exit)")
-# compare input to skills and add score from list of skills if not part of list ask if anything else they use or to end
-for option in skills:
-    if option['code'].lower() == selection.lower():
-        print (f"{option['code']} is {option['score']} points")
+user_input = input("What code skills do you have please separate with commas no spaces e.g. CSS,HTML,Git(nothing to exit)")
+user_skills = []
+skill = ''
 
-# remove from list to not be selected anymore
+for input in user_input:
+    if input != ',':  # gets input and separates between comma and words
+        skill += input
+    else:
+        user_skills.append(skill)
+        skill = ''
+user_skills.append(skill)
+# make non case sensitive
+for i in range(len(user_skills)):
+    user_skills[i] = user_skills[i].lower()
+    
+# print(user_skills)# debug
+# print (skill) # debug
+
+# check score for inputted skills
+score = 0
+for skill in user_skills:
+    if skill in skills:
+        score += skills[skill]
+
 # give user score check if user wants to input more skills if yes repeat
+print(f"Your overall coding skill score is {score}.")
 # if not list out the skills not selected and how much each is worth
 
+suggestions = []
+for skill, value in skills.items(): # contains the key-value pairs of the dictionary, as tuples in a list
+    if skill not in user_skills:
+        suggestions.append(f"{skill} (+{value})")
+if suggestions:
+    print("You may want to learn:")
+    for suggestion in suggestions:
+        print(suggestion)
