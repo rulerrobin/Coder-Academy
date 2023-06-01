a505f5753bd3c67f43446cc0df9d720508c45ac7
Genuel Mark <gmzapanta98@gmail.com>
2023-06-01 10:02:54 +1000

    added tablenames

--- flask-lessons/trello-clone/app.py
+++ flask-lessons/trello-clone/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 
@@ -13,17 +14,40 @@ db = SQLAlchemy(app) # create new instance of SQLAlchemy with a connection to th
 
 # alchemy allows us to put a model on a database
 class Card(db.Model): # inheriting from database SQLalchemy structure
+    __tablename__ = 'cards' # declare name of table
+
     id = db.Column(db.Integer, primary_key=True) # tells it is the primary key and it integer
     title = db.Column(db.String(100))
     description = db.Column(db.String())
     date_created = db.Column(db.Date())
 
 
+# just as per sql tables we dropped first we also need to drop the tables whenever created so it becomes a new slate
 
 @app.cli.command('create') # creates CLI customer commands, string can be anything
 def create_db():
+    db.drop_all() # drops tables for new slate
     db.create_all() # creates the databases that are defined above (as intepreted language)
     print('Tables created successfull')
+
+@app.cli.command('seed') # creates instance of the Card model in memory
+def seed_db():
+    # created new instance of Card
+    card = Card(
+        title = 'Start the project',
+        description = 'Stage 1 - Create an ERD',
+        date_created = date.today()
+    )
+
+    # truncate the Card table same as drop table but only deletes records in table as reseeding
+    db.session.query(Card).delete()
+
+    # add card to session (transaction)
+    db.session.add(card)
+
+    # commit transaction to database 
+    db.session.commit()
+    print('Models seeded')
 
 @app.route('/')
 def index():
