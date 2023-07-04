2bb6d15aaf74ce0b39c0b2d1128dd7d197d8dbe8
Genuel Mark <gmzapanta98@gmail.com>
2023-07-04 18:20:58 +1000

    JS intro lesson

---
+++ javascript/intro/script.js
@@ -0,0 +1,31 @@
+// let str = 'Hello World!'
+
+// console.log(str.replace(/o/g, '---'))
+//  console.log(`Message: ${5 * 10}`)
+
+// ncrement
+// let x = 5
+// console.log(x++)
+// console.log(++x)
+
+// console.log("123" == 123) // Auto type conversion
+
+// person = {
+//     name: 'Matt',
+//     age: 50
+// }
+// console.log(person.name)
+
+x = [1, 2, 3.14159, true, 'hello']
+
+console.log(x.includes('fdadfad'))
+
+// Python
+// def add(x, y): 
+//   return x + y
+
+function add(x, y) {
+    return x + y
+}
+
+console.log(add(5,10))
